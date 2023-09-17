8197911680418b8b4a15d1c789461f93c3c48aca
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2023-09-17 17:28:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-function firstNonRepeatedChar(str) {
+//function firstNonRepeatedChar(str) {
  // Write your code here
+	function firstNonRepeatedChar(str) {
+  // Create an object to store character frequencies
+  const charFrequency = {};
+
+  // Iterate through the string and count character frequencies
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charFrequency[char]) {
+      charFrequency[char]++;
+    } else {
+      charFrequency[char] = 1;
+    }
+  }
+
+  // Iterate through the string again to find the first non-repeated character
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charFrequency[char] === 1) {
+      return char; // Return the first non-repeated character
+    }
+  }
+
+  // If no non-repeated character is found, return null
+  return null;
+}
+
+// Example usage:
+const inputString = "leetcode";
+const result = firstNonRepeatedChar(inputString);
+console.log(result); // Output: "l"
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
